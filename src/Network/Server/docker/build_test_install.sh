a17228bda7b5f37fe39f06fbe6dbb8c621b7d016
Friedrich Doku <friedrichdoku@gmail.com>
2019-03-23 07:59:15 -0500

    Minor changes to source

--- src/Network/Server/docker/build_test_install.sh
+++ src/Network/Server/docker/build_test_install.sh
@@ -19,24 +19,24 @@ wget http://www.openssl.org/source/openssl-1.0.0a.tar.gz
 tar -xf openssl-1.0.0a.tar.gz
 cd openssl-1.0.0a
 ./config --prefix=/usr/local/openssl-1.0
-sudo make
-sudo make install
+make
+make install
 cd ../
 
 cd Mixer
 make
-sudo make install
+make install
 cd ../
 cd PKG
 make
-sudo make install
+make install
 cd ../
 git clone https://github.com/DokuEnterprise/crypto
 cd crypto
 cd stanfordibe
 cp ../../Network/Server/docker/libssl.so.1.0.0 .
 make
-sudo make install
+make install
 
 
 CMD ["/bin/bash"]
