6a4d89b28d8fee99fdda789baf696da236e552ab
Friedrich Doku <friedrichdoku@gmail.com>
2019-03-23 14:15:38 -0500

    Minor change

--- src/Network/Server/docker/build_test_install.sh
+++ src/Network/Server/docker/build_test_install.sh
@@ -16,19 +16,19 @@ mkdir $CPLUS_INCLUDE_PATH/zephyr
 mkdir $CPLUS_INCLUDE_PATH/sibe
 
 # Install OpenDHT dependencies
-sudo apt-get install libncurses5-dev libreadline-dev libjsoncpp-dev nettle-dev libgnutls-dev librestbed-dev libjsoncpp-dev
+apt-get install libncurses5-dev libreadline-dev libjsoncpp-dev nettle-dev libgnutls-dev librestbed-dev libjsoncpp-dev
 
 # Install python binding dependencies
-sudo apt-get install cython3 python3-dev python3-setuptools
+apt-get install cython3 python3-dev python3-setuptools
 
 # Build and install msgpack-c
-sudo apt-get install build-essential cmake
+apt-get install build-essential cmake
 wget https://github.com/msgpack/msgpack-c/releases/download/cpp-2.1.1/msgpack-2.1.1.tar.gz
 tar -xzf msgpack-2.1.1.tar.gz
 cd msgpack-2.1.1 && mkdir build && cd build
 cmake -DMSGPACK_CXX11=ON -DMSGPACK_BUILD_EXAMPLES=OFF -DCMAKE_INSTALL_PREFIX=/usr ..
 make -j4
-sudo make install
+make install
 cd ../../
 
 wget http://www.openssl.org/source/openssl-1.0.0a.tar.gz
