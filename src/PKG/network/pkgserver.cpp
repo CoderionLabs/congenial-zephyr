d70cb6f0b8aa4fb5ee7a03c84a0e02e13a0a0e09
Friedrich Doku <friedrichdoku@gmail.com>
2019-05-11 13:58:15 -0500

    minor change

--- src/PKG/network/pkgserver.cpp
+++ src/PKG/network/pkgserver.cpp
@@ -143,7 +143,7 @@ do_session(tcp::socket& socket)
             send_email(email, code);
 
             // Ask for the code
-            ws.text(ws.got_binary());
+            ws.binary();
             std::string msg = "I sent a code to your email. Please paste it in.";
             ws.write(net::buffer(std::string(msg)));
             os.str("");
