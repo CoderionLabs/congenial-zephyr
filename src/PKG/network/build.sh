0aea1a3673f87692262215cc5de7a53586aea032
Friedrich Doku <friedrichdoku@gmail.com>
2019-05-26 18:04:33 -0500

    Tweak build script

--- src/PKG/network/build.sh
+++ src/PKG/network/build.sh
@@ -1,2 +1,2 @@
-g++ -std=c++11 pkgserver.cpp -lboost_system -lpthread -lpkg -o pkgs
+g++ -std=c++11 pkgserver.cpp -lboost_system -lsibe -lpthread -lpkg -o pkgs
 g++ -std=c++11 pkgclient.cpp -lboost_system -lpthread -lpkg -o pkgc
