af5885215cab1c8296d98242217f1f9b23c82c16
Friedrich Doku <friedrichdoku@gmail.com>
2019-07-25 15:56:29 -0500

    Minor change to start script

--- src/Network/Server/docker/setup.sh
+++ src/Network/Server/docker/setup.sh
@@ -5,11 +5,11 @@
 tab=" --tab"
 options=()
 
-cmds[1]="docker start -a -i demspkg0"
-cmds[2]="docker start -a -i dems0"
-cmds[3]="docker start -a -i dems1"
-cmds[4]="docker start -a -i dems2"
-cmds[5]="docker start -a -i dems3"
+cmds[1]="docker start -a -i dems0"
+cmds[2]="docker start -a -i dems1"
+cmds[3]="docker start -a -i dems2"
+cmds[4]="docker start -a -i dems3"
+cmds[5]="docker start -a -i dems4"
 
 for i in 1 2 3 4 5; do
 options+=($tab -e "bash -c '${cmds[i]} ; bash'" )
