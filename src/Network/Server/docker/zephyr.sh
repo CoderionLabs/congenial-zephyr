e4e08daf7be029690f49e0eca9f826d3ad261a66
Friedrich Doku <friedrichdoku@gmail.com>
2019-08-23 20:44:54 -0500

    Make docker builds faster

---
+++ src/Network/Server/docker/zephyr.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -evu
+
+PREFIX=/usr/local
+
+
+if [ "$OS" == "arch" ] || [ "$OS" == "fedora" ]
+then
+	PREFIX=/usr
+fi
+
+export LD_LIBRARY_PATH=$PREFIX/lib
+export CPLUS_INCLUDE_PATH=$PREFIX/include
+export CURRENT_PATH=.
+
+wget http://www.openssl.org/source/openssl-1.0.0a.tar.gz
+tar -xf openssl-1.0.0a.tar.gz
+cd openssl-1.0.0a
+./config --prefix=/usr/local/openssl-1.0
+make
+make install_sw
+ls
+cd ../
+ls
+cd congenial-zephyr
+git checkout nightly
+cd src
+make
