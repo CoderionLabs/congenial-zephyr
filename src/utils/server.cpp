e35dfd45aede0ae668478943fda4c0c603d9adf7
Friedrich Doku <friedrichdoku@gmail.com>
2019-11-01 18:36:50 -0500

    Minor change

--- src/utils/server.cpp
+++ src/utils/server.cpp
@@ -5,6 +5,8 @@ using namespace node;
 
 
 std::vector<std::string> msgtmp;
+std::vector<std::string> outbox;
+
 
 int main(int argc, char** argv) {
   // Expect only arg: --db_path=path/to/route_guide_db.json.
