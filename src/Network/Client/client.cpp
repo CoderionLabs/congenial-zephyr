2b6485912570edc0241344b46965463e97ef2d25
professor <friedrichdoku@gmail.com>
2020-02-09 12:35:26 -0600

    Minor changes to source

--- src/Network/Client/client.cpp
+++ src/Network/Client/client.cpp
@@ -152,6 +152,7 @@ int main(){
                 auto d = x;
                 std::cout << "THIS IS WHAT I GOT " << std::endl;
                 std::cout << d << std::endl;
+                //TODO: Fix encrytion run a test on the local system first
                 auto j = pkg_decrypt(d, keyb, paramsb);
                 
             }
